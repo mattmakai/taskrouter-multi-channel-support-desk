8496f4c88edb1d73d5ea45b59fd742729d4487f8
Matt Makai <makaimc@gmail.com>
2015-02-17 12:48:46 -0500

    handling tutorial step 3

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import json
 import os
-from flask import Flask, Response
+from flask import Flask, Response, request
 from twilio import twiml
 
 ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
-SUPPORT_AGENT_NUMBER = os.environ.get('SUPPORT_AGENT_NUMBER', '')
+SUPPORT_DESK_NUMBER = os.environ.get('SUPPORT_DESK_NUMBER', '')
+WORKFLOW_SID = os.environ.get('WORKFLOW_SID', '')
 
 app = Flask(__name__)
 
@@ -16,9 +18,19 @@ def working():
 @app.route('/call', methods=['GET', 'POST'])
 def call():
     r = twiml.Response()
-    r.dial(SUPPORT_AGENT_NUMBER)
+    r.enqueue('', workflowSid=WORKFLOW_SID)
     return Response(str(r), content_type='application/xml')
 
+
+@app.route('/assign', methods=['POST'])
+def assign():
+    number = json.loads(request.form['WorkerAttributes'])['phone_number']
+    instruction = {
+        "instruction": "dequeue",
+        "to": number,
+        "from": SUPPORT_DESK_NUMBER
+    }
+    return Response(json.dumps(instruction), content_type='application/json')
 
 if __name__ == '__main__':
     # first attempt to get the PORT environment variable, 
