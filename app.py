dcd441aa2f318cbc24bed14569ab2d4576633903
Matt Makai <makaimc@gmail.com>
2015-02-17 15:10:38 -0500

    adding final tag

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import json
 import os
 from flask import Flask, Response, request
 from twilio import twiml
+from twilio.rest import TwilioRestClient, TwilioTaskRouterClient
+
 
 ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
 SUPPORT_DESK_NUMBER = os.environ.get('SUPPORT_DESK_NUMBER', '')
+WORKSPACE_SID = os.environ.get('WORKSPACE_SID', '')
 WORKFLOW_SID = os.environ.get('WORKFLOW_SID', '')
 
+client = TwilioRestClient(account=ACCOUNT_SID, token=AUTH_TOKEN)
+tr_client = TwilioTaskRouterClient(account=ACCOUNT_SID, token=AUTH_TOKEN)
 app = Flask(__name__)
 
 
@@ -24,6 +29,16 @@ def call():
 
 @app.route('/assign', methods=['POST'])
 def assign():
+    task_attrs = json.loads(request.form['TaskAttributes'])
+    if 'training' in task_attrs and task_attrs['training'] == 'sms':
+        number = json.loads(request.form['WorkerAttributes'])['phone_number']
+        instruction = {"instruction": "accept"}
+        client.messages.create(from_=SUPPORT_DESK_NUMBER, to=number,
+            body='Text {0} asking "{1}"'.format(task_attrs['phone_number'],
+                                                task_attrs['body']))
+        return Response(json.dumps(instruction),
+                        content_type='application/json')
+    # defaults to voice call
     number = json.loads(request.form['WorkerAttributes'])['phone_number']
     instruction = {
         "instruction": "dequeue",
@@ -31,6 +46,35 @@ def assign():
         "from": SUPPORT_DESK_NUMBER
     }
     return Response(json.dumps(instruction), content_type='application/json')
+
+
+@app.route('/message', methods=['POST'])
+def message():
+    # check if one of our workers is completing a task
+    if request.form['Body'] == 'DONE':
+        from_number = request.form['From']
+        for w in tr_client.workers(WORKSPACE_SID).list():
+            if from_number == json.loads(w.attributes)['phone_number']:
+                # update worker status back to idle
+                for activity in tr_client.activities(WORKSPACE_SID).list():
+                    if activity.friendly_name == 'Idle':
+                        w.update(activity_sid=activity.sid)
+                        break
+                r = twiml.Response()
+                r.message("Ticket closed.")
+                return Response(str(r), content_type='application/xml')
+
+    task_attributes = {
+        "training" : "sms",
+        "phone_number" : request.form['From'],
+        "body": request.form['Body']
+    }
+    tasks = tr_client.tasks(WORKSPACE_SID).create(json.dumps(task_attributes),
+                                                  WORKFLOW_SID)
+    r = twiml.Response()
+    r.message("Thanks. You'll hear back from us soon.")
+    return Response(str(r), content_type='application/xml')
+
 
 if __name__ == '__main__':
     # first attempt to get the PORT environment variable, 
